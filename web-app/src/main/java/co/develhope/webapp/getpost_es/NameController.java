131819a77c9a6993da485e248d2a2fbbf3e0cb8c
Giovanni <giovanni.zaffinelli99@gmail.com>
2024-05-14 17:00:15 +0200

    get, post, postman, cross origin e documentazione con springdoc

--- web-app/src/main/java/co/develhope/webapp/getpost_es/NameController.java
+++ web-app/src/main/java/co/develhope/webapp/getpost_es/NameController.java
@@ -1,14 +1,18 @@
 package co.develhope.webapp.getpost_es;
-
+import jdk.jfr.Description;
 import org.springframework.web.bind.annotation.*;
+import io.swagger.v3.oas.annotations.Operation;
 
-@RequestMapping("/v2")
+@RequestMapping("/v3")
 @RestController
-@CrossOrigin(origins = "http://localhost:9000")
+// Esercizio cross
+@CrossOrigin(origins ="9000")
 public class NameController {
 
     // il name ="" della RequestParam sarà la variabile locale
     @GetMapping("/name")
+    @Description("Il metodo restituisce il valore del parametro")
+    @Operation(summary="Riassunto più riassuntivo")
     public String nome(@RequestParam String nome) {
         return nome;
     }
@@ -18,9 +22,9 @@ public class NameController {
     }
 
     // PostMan
-    // GET http://localhost:9000/v2/name?nome=Peppe
+    // GET http://localhost:9000/v3/name?nome=Peppe
     // Peppe
 
-    // POST http://localhost:9000/v2/name?nome=Peppe
+    // POST http://localhost:9000/v3/name?nome=Peppe
     //  eppeP
 }
