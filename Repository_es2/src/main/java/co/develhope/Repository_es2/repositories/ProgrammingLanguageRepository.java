6f1dbab3149c897ffdf098d34f2fa092d777a455
Giovanni <giovanni.zaffinelli99@gmail.com>
2024-05-19 16:12:21 +0200

    HAL rest repository e test con postman

--- Repository_es2/src/main/java/co/develhope/Repository_es2/repositories/ProgrammingLanguageRepository.java
+++ Repository_es2/src/main/java/co/develhope/Repository_es2/repositories/ProgrammingLanguageRepository.java
@@ -1,10 +1,14 @@
 package co.develhope.Repository_es2.repositories;
 
 import co.develhope.Repository_es2.entities.Programminglanguage;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-@RepositoryRestResource(path = "repo-prog-languages", collectionResourceRel = "linguaggi di programmazione", itemResourceRel = "linguaggio di programmazione")
+import java.awt.print.Pageable;
+
+@RepositoryRestResource(path = "linguaggi", collectionResourceRel = "linguaggi di programmazione",
+        itemResourceRel = "linguaggio di programmazione")
 public interface ProgrammingLanguageRepository extends JpaRepository<Programminglanguage,Long> {
+    Page<Programminglanguage> findAll(Pageable pageable);
 }
-// come dimostro che ho usato postman?
