481b13addf6f153a9d74cfe85b0fc4909c506718
Giovanni <giovanni.zaffinelli99@gmail.com>
2024-06-08 19:01:13 +0200

    Controller, port, version, jar and active profiles

---
+++ Deploy_es2/src/main/java/co/develhope/Deploy_es2/DeployEs2Application.java
@@ -0,0 +1,13 @@
+package co.develhope.Deploy_es2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployEs2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeployEs2Application.class, args);
+	}
+
+}
---
+++ Deploy_es2/src/test/java/co/develhope/Deploy_es2/DeployEs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.Deploy_es2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeployEs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
