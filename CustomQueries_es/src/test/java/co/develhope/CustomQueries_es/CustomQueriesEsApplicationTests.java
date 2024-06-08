0d5062ba4e1ec8674248237ab5803d2f9fa2ca42
Giovanni <giovanni.zaffinelli99@gmail.com>
2024-06-08 19:36:11 +0200

    Custom queries per voli generati casualmente

---
+++ CustomQueries_es/src/test/java/co/develhope/CustomQueries_es/CustomQueriesEsApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.CustomQueries_es;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomQueriesEsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ CustomQueries_es2/CustomQueries_es2/src/test/java/co/develhope/CustomQueries_es2/CustomQueriesEs2ApplicationTests.java
@@ -0,0 +1,13 @@
+package co.develhope.CustomQueries_es2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomQueriesEs2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
