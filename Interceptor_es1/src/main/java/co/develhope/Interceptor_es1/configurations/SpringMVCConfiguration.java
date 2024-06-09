be9c33d2bb4223b545442c0d0c6b31313113732c
Giovanni <giovanni.zaffinelli99@gmail.com>
2024-06-09 18:47:10 +0200

    Due interceptor in cui nella prehandle uno restituisce l'user-agent dall'header di una richiesta http e l'altro blocca la chiamata al controller restituendo l'errore 410 gone

--- Interceptor_es1/src/main/java/co/develhope/Interceptor_es1/configurations/SpringMVCConfiguration.java
+++ Interceptor_es1/src/main/java/co/develhope/Interceptor_es1/configurations/SpringMVCConfiguration.java
@@ -3,9 +3,11 @@ package co.develhope.Interceptor_es1.configurations;
 import co.develhope.Interceptor_es1.interceptors.ApiLoggingInterceptor;
 import co.develhope.Interceptor_es1.interceptors.LegacyInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class SpringMVCConfiguration implements WebMvcConfigurer {
     @Autowired
     ApiLoggingInterceptor apiLoggingInterceptor;
--- Interceptor_es1/src/main/java/co/develhope/Interceptor_es1/controllers/LegacyController.java
+++ Interceptor_es1/src/main/java/co/develhope/Interceptor_es1/controllers/LegacyController.java
@@ -1,9 +1,14 @@
 package co.develhope.Interceptor_es1.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/Legacy")
 public class LegacyController {
+    @GetMapping
+    public String getLegacy(){
+        return "This is just old code";
+    }
 }
