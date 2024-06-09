dec3dce4b2201d90e6306c7f719c1b6bac5fca2b
Giovanni <giovanni.zaffinelli99@gmail.com>
2024-06-09 21:02:53 +0200

    Provisioning of 50 random flights

---
+++ CustomQueries_es1/src/main/java/co/develhope/CustomQueries_es1/CustomQueriesEs1Application.java
@@ -0,0 +1,13 @@
+package co.develhope.CustomQueries_es1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomQueriesEs1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustomQueriesEs1Application.class, args);
+	}
+
+}
