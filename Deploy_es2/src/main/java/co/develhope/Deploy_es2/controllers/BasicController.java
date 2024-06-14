3d7476c8419537db4f5d6872fe33328e920b4709
Giovanni <giovanni.zaffinelli99@gmail.com>
2024-06-14 18:57:28 +0200

    Controller, port, version, jar and active profiles

--- Deploy_es2/src/main/java/co/develhope/Deploy_es2/controllers/BasicController.java
+++ Deploy_es2/src/main/java/co/develhope/Deploy_es2/controllers/BasicController.java
@@ -1,11 +1,13 @@
 package co.develhope.Deploy_es2.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Random;
 
 @RestController
 public class BasicController {
+    @GetMapping
     public int getRandInt(){
         Random random = new Random();
         int firstNum = random.nextInt(10);
